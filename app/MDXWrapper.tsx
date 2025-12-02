22be77814a0900f49ccab4bdb2ab88e1336ad697
dlacowns21 <dlacowns21@naver.com>
2025-12-02 19:12:04 +0900

    fix: MDXWrapper children 문법 수정

--- app/MDXWrapper.tsx
+++ app/MDXWrapper.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { MDXProvider } from "@mdx-js/react";
+import type { ReactNode } from "react";
 import { mdxComponents } from "./mdx-components";
 
-export default function MDXWrapper({ children }) {
+type MDXWrapperProps = {
+  children: ReactNode;
+};
+
+export default function MDXWrapper({ children }: MDXWrapperProps) {
   return (
     <MDXProvider components={mdxComponents}>
-      <div className="prose prose-invert max-w-none">
-        {children}
-      </div>
+      <div className="prose prose-invert max-w-none">{children}</div>
     </MDXProvider>
   );
 }
